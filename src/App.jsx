a41696dc60ccccbe3bdb8016fc65089127c74080
Abraham Ramirez <110066308+SonOfMovich@users.noreply.github.com>
2023-02-14 19:03:47 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import ItemListContainer from "./ItemListContainer";
+import Navbar from "./Navbar";
+
+function App(){
+    return (
+        <>
+        <Navbar />
+        <ItemListContainer />
+        </>
+    )
+}
+
+export default App;
